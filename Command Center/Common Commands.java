8dc09cd74506ed0f1601a9423f217ec5ab2bfe72
Drew Hartzell <151415194+drewhartzell@users.noreply.github.com>
2025-06-10 14:05:35 -0400

    Update and rename Common Commands.py to Common Commands.java

---
+++ Command Center/Common Commands.java
@@ -0,0 +1,30 @@
+// For Command Prompt navigation //
+// Utilized w/ bash or Command Prompt //
+
+// cd Navigate to directory:
+  // C:\Users\xxxxx\AppData> 
+    cd Local
+  // C:\Users\xxxxx\AppData\Local\Programs\Python> 
+    cd Python313 -- etc.
+
+//
+//
+
+// Install libraries to commonly used site local
+  pip install pandas
+  pip show pandas
+  pip install matplotlib
+
+// Install libraries to exact library/location specified (navigation is required first - see above) -- 
+  python -m pip install pandas matplotlib
+  python -m pip install scikit-learn prophet
+
+
+//
+//
+
+// where python: Where is Python downloaded?
+  python -m site
+  // Where are the site-packages located?
+  python -m pip list
+  // : List all of the site-packages (is pandas, numpy, or matplotlib in the libraries)
